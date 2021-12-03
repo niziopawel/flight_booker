8297d852fb1c5ca9db2df7ba5cb3761b4bcf3411
niziopawel <niziopawel2@gmail.com>
2021-12-03 15:21:03 +0100

    Rename db namings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "odin_flight_booker_production"
+  # config.active_job.queue_name_prefix = "flight_booker_production"
 
   config.action_mailer.perform_caching = false
 
